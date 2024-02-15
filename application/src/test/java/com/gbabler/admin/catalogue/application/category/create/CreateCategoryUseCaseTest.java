4e2bf8a5daf8a51fc1253252a55fe985f98cda45
Gabriel Babler <gjacobbabler@atlassian.com>
2024-02-15 20:42:28 -0300

    add beforeEach method to clean up mock

--- application/src/test/java/com/gbabler/admin/catalogue/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/gbabler/admin/catalogue/application/category/create/CreateCategoryUseCaseTest.java
@@ -2,6 +2,7 @@ package com.gbabler.admin.catalogue.application.category.create;
 
 import com.gbabler.admin.catalogue.domain.category.CategoryGateway;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -26,6 +27,11 @@ public class CreateCategoryUseCaseTest {
 
     @Mock
     private CategoryGateway categoryGateway;
+
+    @BeforeEach
+    void cleanUp() {
+        Mockito.reset(categoryGateway);
+    }
 
     @Test
     void givenAValidCommand_whenCallsCreateCategory_thenShouldReturnCategoryId() {
