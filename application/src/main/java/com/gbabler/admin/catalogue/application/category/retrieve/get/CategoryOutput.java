dfe822dc956a1bf7b908988ae24dec97710b4357
Gabriel Babler <gjacobbabler@atlassian.com>
2024-02-15 21:18:30 -0300

    implement getById and tests

---
+++ application/src/main/java/com/gbabler/admin/catalogue/application/category/retrieve/get/CategoryOutput.java
@@ -0,0 +1,28 @@
+package com.gbabler.admin.catalogue.application.category.retrieve.get;
+
+import com.gbabler.admin.catalogue.domain.category.Category;
+import com.gbabler.admin.catalogue.domain.category.CategoryID;
+
+import java.time.Instant;
+
+public record CategoryOutput(
+        CategoryID id,
+        String name,
+        String description,
+        boolean isActive,
+        Instant createdAt,
+        Instant updatedAt,
+        Instant deletedAt
+) {
+    public static CategoryOutput from(Category aCategory) {
+        return new CategoryOutput(
+                aCategory.getId(),
+                aCategory.getName(),
+                aCategory.getDescription(),
+                aCategory.isActive(),
+                aCategory.getCreatedAt(),
+                aCategory.getUpdatedAt(),
+                aCategory.getDeletedAt()
+        );
+    }
+}
---
+++ application/src/main/java/com/gbabler/admin/catalogue/application/category/retrieve/get/GetCategoryByIdUseCase.java
@@ -0,0 +1,6 @@
+package com.gbabler.admin.catalogue.application.category.retrieve.get;
+
+import com.gbabler.admin.catalogue.application.UseCase;
+
+public abstract class GetCategoryByIdUseCase extends UseCase<String, CategoryOutput> {
+}
