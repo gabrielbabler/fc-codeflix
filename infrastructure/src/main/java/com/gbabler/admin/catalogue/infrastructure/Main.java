f0b921de4ae5a93112ea47ada7935fd44b6fcb93
Gabriel Babler <gjacobbabler@atlassian.com>
2024-02-22 20:39:34 -0300

    starting implementing infra layer

--- infrastructure/src/main/java/com/gbabler/admin/catalogue/infrastructure/Main.java
+++ infrastructure/src/main/java/com/gbabler/admin/catalogue/infrastructure/Main.java
@@ -1,9 +1,13 @@
 package com.gbabler.admin.catalogue.infrastructure;
 
-import com.gbabler.admin.catalogue.application.UseCase;
+import com.gbabler.admin.catalogue.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infrastructure/src/main/java/com/gbabler/admin/catalogue/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.gbabler.admin.catalogue.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.gbabler.admin.catalogue")
+public class WebServerConfig {
+}
