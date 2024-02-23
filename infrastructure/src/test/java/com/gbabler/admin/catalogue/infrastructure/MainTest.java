8aa17b9a7438b0e8b235035f5991ff0f3283f3a8
Gabriel Babler <gjacobbabler@atlassian.com>
2024-02-22 21:33:54 -0300

    adding test config and h2

---
+++ infrastructure/src/test/java/com/gbabler/admin/catalogue/infrastructure/MainTest.java
@@ -0,0 +1,15 @@
+package com.gbabler.admin.catalogue.infrastructure;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.env.AbstractEnvironment;
+
+public class MainTest {
+
+    @Test
+    void testMain() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
+        Assertions.assertNotNull(new Main());
+        Main.main(new String[]{});
+    }
+}
